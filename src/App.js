b460186cc16837f774130bf019d81c4fa4e9557e
fyrodems <fyrodems@gmail.com>
2022-10-02 19:02:46 +0200

    reorganize js filres for home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header';
-import Home from './components/Home';
+import Home from './components/homePage';
 
 const App = () => {
   return (
