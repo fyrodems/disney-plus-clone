60c6a47c3ac484f6abfad32502a8dbe7898974ca
fyrodems <fyrodems@gmail.com>
2022-10-03 21:15:55 +0200

    add react router dom

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 import Header from './components/header';
 import Home from './components/homePage';
+import Detail from './components/detailPage'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+
 
 const App = () => {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route exact path="/detail" element={<Detail/>} />
+          <Route exact path="/" element={<Home/>} />
+        </Routes>
+      </Router>
   );
 }
 
