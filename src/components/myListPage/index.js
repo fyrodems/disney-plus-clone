0c7e2a7f2c9acfeaa26244c35d742de5f6fbf6b9
fyrodems <fyrodems@gmail.com>
2025-03-09 20:53:58 +0100

    fix my list render

--- src/components/myListPage/index.js
+++ src/components/myListPage/index.js
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
-
-import MovieCard from "../common/MovieCard";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const MyList = () => {
   const myList = useSelector((state) => state.myList.myList);
@@ -8,18 +8,54 @@ const MyList = () => {
   console.log(myList);
 
   return (
-    <>
+    <Container>
+      <h2>Moja lista</h2>
       {myList.length === 0 ? <p>Brak filmów w liście</p> : null}
-      {myList.map((movie) => (
-        <MovieCard
-          key={movie.title}
-          linkto={`/detail/${movie.id}`}
-          src={movie.cardImg}
-          alt={movie.title}
-        />
-      ))}
-    </>
+      <Content>
+        {myList.map((movie, key) => (
+          <MovieCard key={key}>
+            <Link to={`/detail/${movie.id}`}>
+              <img src={movie.cardImg} alt={movie.title} />
+            </Link>
+          </MovieCard>
+        ))}
+      </Content>
+    </Container>
   );
 };
 
 export default MyList;
+
+const Container = styled.div`
+  padding: 0px calc(5px + 3.5vw);
+`;
+
+const Content = styled.div`
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
+`;
+
+const MovieCard = styled.div`
+  border-radius: 10px;
+  overflow: hidden;
+  cursor: pointer;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  :hover {
+    transform: scale(1.05);
+    border: 3px solid rgba(249, 249, 249, 0.8);
+  }
+`;
